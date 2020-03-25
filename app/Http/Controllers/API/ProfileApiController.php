df9c2453ab5c41455120991008dcd763bb28a248
Tayeb-Ali <Mr.elteb@gmail.com>
2020-03-25 16:38:20 +0200

    update #10 fcm

--- app/Http/Controllers/API/ProfileApiController.php
+++ app/Http/Controllers/API/ProfileApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
 
 class ProfileApiController extends Controller
@@ -13,6 +14,18 @@ class ProfileApiController extends Controller
         $user = auth('api')->user();
         if ($user) {
             return response()->json(['status' => true, 'user' => $user]);
+        }
+        return response()->json(['status' => false]);
+    }
+
+    public function updateFCM(Request $request)
+    {
+        $user = auth('api')->user();
+        if ($user) {
+            $userModel = User::find($user->id);
+            $userModel->fcm_registration_id = $request->fcm_registration_id;
+            $userModel->save();
+            return response()->json(['status' => true, 'user' => $userModel]);
         }
         return response()->json(['status' => false]);
     }
